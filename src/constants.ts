07973c135f63fd7bbd64f29bf639eab8e46a42ac
Bob <>
2023-05-10 00:00:02 +0800

    open starknet

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 
 const TABS = [
   { name: 'cairo', url: 'https://cairo-book.github.io/', isOpen: true },
-  { name: 'starknet', url: 'https://book.starknet.io/', isOpen: false },
+  { name: 'starknet', url: 'https://book.starknet.io/', isOpen: true },
   { name: 'rust', url: 'https://doc.rust-lang.org/book/', isOpen: true },
 ]
 
